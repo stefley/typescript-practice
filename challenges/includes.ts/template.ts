0e7d9f31347371d36ba08c90cbba0d28b00ed213
zyq <zyq6227738@163.com>
2022-03-17 09:23:52 +0800

    includes

---
+++ challenges/includes.ts/template.ts
@@ -0,0 +1,5 @@
+type Equal<X,Y> = 
+    (<T>() => T extends X ? 1 : 2) extends 
+        (<T>() => T extends Y ? 1 : 2) ? true : false;
+
+type Includes<T extends any[], U> = T extends [infer F, ...infer R] ?  Equal<F,U> extends true ? true : Includes<R, U> : false;
