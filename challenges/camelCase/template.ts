44771b9836a37029025a8451f4555503f09bfd6c
zyq <zyq6227738@163.com>
2022-04-25 09:48:18 +0800

    camel case

--- challenges/camelCase/template.ts
+++ challenges/camelCase/template.ts
@@ -1,26 +1,34 @@
-type CamelCase<S extends string> = 
-  S extends "" 
-  ? 
-  "" 
-  :
-  S extends `${infer A}${infer B}` 
-    ? 
-    A extends '-' 
-      ?
-      B extends `${infer C}${infer D}`
-        ?
-        C extends Uppercase<C> 
-          ?
-          `${A}${CamelCase<B>}`
-          :
-          `${Uppercase<C>}${CamelCase<D>}`
-        :
-        B extends Uppercase<B>
-          ?
-          `-${B}`
-          :
-          `${Uppercase<B>}`
-      :
-      `${A}${CamelCase<B>}`
-    :
-    S
+// type CamelCase<S extends string> = 
+//   S extends "" 
+//   ? 
+//   "" 
+//   :
+//   S extends `${infer A}${infer B}` 
+//     ? 
+//     A extends '-' 
+//       ?
+//       B extends `${infer C}${infer D}`
+//         ?
+//         C extends Uppercase<C> 
+//           ?
+//           `${A}${CamelCase<B>}`
+//           :
+//           `${Uppercase<C>}${CamelCase<D>}`
+//         :
+//         B extends Uppercase<B>
+//           ?
+//           `-${B}`
+//           :
+//           `${Uppercase<B>}`
+//       :
+//       `${A}${CamelCase<B>}`
+//     :
+//     S
+
+type FirstCamel<T extends string> = T extends `${infer F}${infer R}` ? `${Uppercase<F>}${R}` : T
+
+type CamelCase<S extends string> = S extends `${infer F}-${infer R}`
+    ? R extends FirstCamel<R>
+        ? `${F}-${CamelCase<R>}`
+        : `${F}${CamelCase<FirstCamel<R>>}`
+    : S
