de34615ca98f9fd25abd2ddf98180038add6e5eb
zyq <zyq6227738@163.com>
2022-04-26 09:58:48 +0800

    diff

---
+++ challenges/diff/template.ts
@@ -0,0 +1,5 @@
+type Diff<O, O1> = {
+    [Key in Exclude<keyof O, keyof O1> | Exclude<keyof O1, keyof O>]: Key extends keyof O ? O[Key] : Key extends keyof O1 ? O1[Key] : never
+}
+
+// type Exclude<T,U> = T extends U ? never : T;
