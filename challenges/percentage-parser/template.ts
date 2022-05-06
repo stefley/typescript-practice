2f494f513812f68556b849ee0d708e64b19480cf
zyq <zyq6227738@163.com>
2022-05-06 10:10:30 +0800

    percentage parser

---
+++ challenges/percentage-parser/template.ts
@@ -0,0 +1,1 @@
+type PercentageParser<T extends string, A extends any[] = ['', '', '']> = T extends `${infer L}${infer R}` ? L extends '+' | '-' ? PercentageParser<R, [L, A[1], A[2]]> : L extends '%' ? [A[0], A[1], L] : PercentageParser<R, [A[0], `${A[1]}${L}`, A[2]]> : A
