fcc0cfa1b188992458b3f72edbc3bc3c2d899ac5
zyq <zyq6227738@163.com>
2022-07-22 09:49:47 +0800

    map types

---
+++ challenges/map-types/template.ts
@@ -0,0 +1,13 @@
+type MapTypes<
+  T extends Record<keyof any, unknown>,
+  R extends {
+    mapFrom: unknown;
+    mapTo: unknown;
+  }
+> = {
+  [K in keyof T]: R extends any ? (R['mapFrom'] extends T[K] ? R['mapTo'] : never) : never;
+} extends infer U
+  ? {
+      [K in keyof U]: [U[K]] extends [never] ? T[K] : U[K];
+    }
+  : never;
